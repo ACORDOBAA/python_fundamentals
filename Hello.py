905809a1c5cb7dbfb8a1e909efa7abc1c7ce52d0
ACORDOBAA <139387687+ACORDOBAA@users.noreply.github.com>
2023-07-13 12:42:26 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,2 +1,10 @@
 print("Mi nombre es Agustin Cordoba")
 print("El éxito consiste en ir de fracaso en fracaso sin perder el entusiasmo")
+
+a = ("Hola Mundo")
+print(a)
+
+# El texto es STRING como en DAX, por lo que tiene que ir entre comillas. Sino puede ir sin 
+
+edad = 25
+print(edad)
